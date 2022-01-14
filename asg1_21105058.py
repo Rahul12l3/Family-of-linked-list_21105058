4bd00fa3e7bc69c54198bd835462e9a692d64947
Rahul12l3 <97743791+Rahul12l3@users.noreply.github.com>
2022-01-14 22:14:48 +0530

    Add files via upload

---
+++ asg1_21105058.py
@@ -0,0 +1,46 @@
+          #Assignment 1        
+
+                     #1st program
+print("                    1st program")
+number1=float(input("enter first number: "))
+number2=float(input("enter second number: "))
+number3=float(input("enter third number: "))
+average=(number1+number2+number3)/3
+print("average is ",average
+
+                     #2nd program
+print("                    2nd program")
+rate=0.2
+standard_deduction=10000
+dependent_deduction=3000
+Gross_income=float(input("enter your gross income: "))
+Number_of_dependents=int(input("enter number of dependents: "))
+Tax=float((Gross_income-standard_deduction-3000*Number_of_dependents)*rate)
+print("Tax is: $",Tax)
+
+                     #3rd program
+print("                    3rd program")
+print("Student=[SID, Name, Gender, Course Name, CGPA]")
+Student=[21105058, 'Rahul', 'M', 'ECE', 8.5]
+print("Student= ",Student)
+
+
+                     #4th program
+print("                    4th program")
+print("Marks of five students")
+Marks=[88,85,67,22,49]
+print(" List of marks before sorting",Marks)
+Marks.sort()
+print(" List of marks after sorting",Marks)
+
+                      #5th program
+print("                    5th program (a)")
+colour=['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
+colour.pop(3)
+print(colour)
+
+                       #5th program
+print("                    5th program(b)")
+colour=['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
+colour[3:5]=['Purple']
+print(colour)
