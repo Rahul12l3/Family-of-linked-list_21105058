105dba0918f5fb94003769ee3593611c252d02e6
Rahul12l3 <97743791+Rahul12l3@users.noreply.github.com>
2022-03-02 00:23:04 +0530

    Add files via upload

---
+++ output_asign4.py
@@ -0,0 +1,56 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+================= RESTART: F:/PythonProject/asign4_21105058.py =================
+                    1st answer
+move the disk 1 from source a to destination b
+move the disk 2 from source a to destination c
+move the disk 1 from source b to destination c
+move the disk 3 from source a to destination b
+move the disk 1 from source c to destination a
+move the disk 2 from source c to destination b
+move the disk 1 from source a to destination b
+                    2nd answer(a)
+enter number of rows5
+        1   
+      1   1   
+    1   2   1   
+  1   3   3   1   
+1   4   6   4   1   
+
+                    2nd answer(b)
+enter no of rows: 5
+        1   
+      1   1   
+    1   2   1   
+  1   3   3   1   
+1   4   6   4   1   
+                    3rd answer
+enter first number: 10
+enter second number: 20
+quotient and remainder of the numbers are:  (0, 10)
+                     part(a)
+False
+                     part(b)
+all the values are non zero
+                     part(c)
+values greater than 4 are [5, 6, 10]
+[5, 6, 10]
+                     part(d)
+                     part(e)
+after making set immutable: frozenset({10, 5, 6})
+maximum value of the set is: 10
+                      part(f)
+hash value of set considering it to be an integer: 10
+hash value of set considering it to be a string:  8185019329460075713
+                    4th question
+object is created
+student's name: Rahul and sid is: 21105058
+object destroyed
+                    5th answer
+updated salary of employee Mehak is 70000
+record of employee Viren deleted
+                    6th answer
+enter word said by george:best
+enter word that barbie uttered:setb
+barbie and george are best friends
