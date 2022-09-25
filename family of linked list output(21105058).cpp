560c994e87ed289b550fb03c593dfc7c795ae314
Rahul <97743791+Rahul12l3@users.noreply.github.com>
2022-09-25 23:22:44 +0530

    Create family of linked list output(21105058).cpp

---
+++ family of linked list output(21105058).cpp
@@ -0,0 +1,26 @@
+PS C:\c++> cd
+
+
+
+
+
+
+
+
+
+
+
+         > cd "c:\c++\" ; if ($?) { g++ tempCodeRunnerFile.cpp -o tempCodeRunnerFile } ; if ($?) { .\tempCodeRunnerFile }
+Enter the number of family members:4
+Enter family member number1 name:father
+Enter family member number 1 age:49
+Enter family member number2 name:mother
+Enter family member number 2 age:45
+Enter family member number3 name:sister
+Enter family member number 3 age:20
+Enter family member number4 name:brother
+Enter family member number 4 age:17
+
+The Doubly Linked list is shown below:
+
+[Name:father Age:49]<=>[Name:mother Age:45]<=>[Name:sister Age:20]<=>[Name:brother Age:17]
