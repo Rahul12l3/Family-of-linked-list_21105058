c297b6a8ab5623bff268b9c9b6e4160f854ede72
Rahul <97743791+Rahul21105058@users.noreply.github.com>
2022-10-16 23:38:27 +0530

    Create size_unknown_21105058.cpp

---
+++ size_unknown_21105058.cpp
@@ -0,0 +1,62 @@
+//name-->rahul
+//sid-->21105058
+
+#include <iostream>         
+using namespace std;         
+
+
+// QUESTION:How would you search for an element in an array/list whose size is unknown?
+
+int binary_search(int arr[],int i,int j,int key){
+
+    int m=(i+j)/2;
+
+    while(i<=j){
+    if(arr[m]==key){
+      return m;           // if key to be searched is present we will return index of the key
+    }
+
+    else{
+        if(arr[m]<key){
+          i=m+1;
+        }
+
+        else{
+          j=m-1;
+        }
+
+    }
+    m=(i+j)/2;
+
+    }
+    return -1;       // if element is not present we will return -1
+}
+
+//modified Binary search for array with unknown lenght
+int binary_search_unknown(int arr[],int key){
+    //Initializing i as 0 and j as 1.
+    int i=0,j=1;
+    while(arr[j]<key){
+        i=j;
+        j=2*j;
+    }
+    //Now calling Simple Binary Search on range i to j.
+    return binary_search(arr,i,j,key);
+}
+
+int main(){
+    int n;
+    cin>>n;
+    
+    int*arr=new int[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+
+    int key;
+    cin>>key;
+    int element=binary_search_unknown(arr,key);
+    cout<<element;
+    return 0;
+    delete[] arr;
+}
